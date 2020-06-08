7889990bd45ced1827518fc45e0d54afd7e5029e
Vedant Chavda <pointoflight@Vedants-MacBook-Pro.local>
2020-06-09 01:28:57 +0530

    add deformations.py, complete net.py

--- net.py
+++ net.py
@@ -9,9 +9,12 @@ from keras.layers import (
 )
 
 def generate_convolutional_block(inp, filters, length=2, pool=True, stride=1):
+    "Generates a convolutional block, with a couple of simple options"
+
     output = inp
     
-     for i in range(length):
+    for i in range(length):
+        # convolution
         output = Conv2D(
             filters=filters,
             kernel_size=3,
@@ -20,7 +23,10 @@ def generate_convolutional_block(inp, filters, length=2, pool=True, stride=1):
             kernel_initializer='he_normal'
         )(output)
 
-        output = BatchNormalization()(output)       
+        # batch normalization
+        output = BatchNormalization()(output)
+
+        # ReLU       
         output = Activation('relu')(output)
         
     shortcut = Conv2D(
@@ -31,12 +37,15 @@ def generate_convolutional_block(inp, filters, length=2, pool=True, stride=1):
             kernel_initializer='he_normal'
         )(inp)
     
+    # batch normalization
     shortcut = BatchNormalization()(shortcut)
+
+    # ReLU
     shortcut = Activation('relu')(shortcut)
     
     output = (output + shortcut) / 2.
     
-    if (pool):      
+    if pool:      
         output = MaxPooling2D(
             pool_size=3,
             strides = 2
@@ -58,7 +67,7 @@ def generate_convolutional_block(inp, filters, length=2, pool=True, stride=1):
         output = generate_convolutional_block(output, filters=80*width, pool=False)
 
         # Global average pooling
-        output = GlobalAveragePooling2D(data_format='channels_last' name='gap')(output)
+        output = GlobalAveragePooling2D(data_format='channels_last')(output)
         
         output = Dense(
             units=2, # 2 outputs
@@ -68,4 +77,28 @@ def generate_convolutional_block(inp, filters, length=2, pool=True, stride=1):
 
         probabilities = Activation('softmax', name='probabilities')(output)
 
+        classes = keras.argmax(logits, axis=1, name='classes')
 
+        return inp, labels, {
+        'logits': logits,
+        'probabilities': probabilities,
+        'classes': classes,
+    }
+
+def generate_functions(inp, labels, output):
+    """Generates functions like error, accuracy and train, 
+    that are used for training and testing the network"""
+
+    #error = tf.losses.sparse_softmax_cross_entropy(labels, output['logits'])
+    error = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=output['logits'])
+
+    #optimizer = tf.train.AdamOptimizer(learning_rate=8e-5)
+    optimizer = keras.optimizers.Adam(decay=8e-5)
+    train = optimizer.minimize(error)
+
+    metrics = {
+        'accuracy': tf.metrics.accuracy(labels, output['classes']),
+        'AUC': tf.metrics.auc(labels, output['probabilities'][:,1]),
+    }
+
+    return error, train, metrics
